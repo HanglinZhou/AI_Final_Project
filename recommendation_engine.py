dc97ddea953a43e34e92d773b4ba5868cefe0fb3
hanglinzhou <hanglin.zhou@richmond.edu>
2020-04-28 11:56:24 -0700

    fix typo

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -7,7 +7,9 @@ from pathlib import Path
 data_path = Path("./data/movieLens/")
 tags = pd.read_csv(data_path/"tags.csv")
 links = pd.read_csv(data_path/"links.csv")
-movies = pd.read_csv(data_path/"movies_metadata.csv")
+movies = pd.read_csv(data_path/"movies.csv")
 ratings = pd.read_csv(data_path/"ratings.csv")
 genome_tags = pd.read_csv(data_path/"genome-tags.csv")
 genome_scores = pd.read_csv(data_path/"genome-scores.csv")
+
+print(tags.head())
