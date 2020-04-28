391b27f16ba6e25a4ef0abf9ee4a50defa25fa1b
hanglinzhou <hanglin.zhou@richmond.edu>
2020-04-28 11:51:33 -0700

    add dataset

---
+++ recommendation_engine.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
+#load dataset
+data_path = Path("./data/movieLens/")
+tags = pd.read_csv(data_path/"tags.csv")
+links = pd.read_csv(data_path/"links.csv")
+movies = pd.read_csv(data_path/"movies_metadata.csv")
+ratings = pd.read_csv(data_path/"ratings.csv")
+genome_tags = pd.read_csv(data_path/"genome-tags.csv")
+genome_scores = pd.read_csv(data_path/"genome-scores.csv")
