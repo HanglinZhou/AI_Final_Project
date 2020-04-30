472180f295572d5a6975da0fdde92e4f2288e5e6
hanglinzhou <hanglin.zhou@richmond.edu>
2020-04-30 14:50:17 -0700

    added MF algo outline

--- matrix_factorization_algo.py
+++ matrix_factorization_algo.py
@@ -1,3 +1,15 @@
-# This class contains all Matrix Factorization algorithms that we want to explore for our recommander system.
+"""
+This class contains all Matrix Factorization algorithms that we want to explore for our recommander system.
+algorithms included:
+1. SVD
+2. SVDpp (SVD++): taking into account implicit ratings
+3. PMF (Probabilistic Matrix Factorization): setting "biased" parameter to False
+4. NMF (Non-negative Matrix Factorization)
+
+for SVD and SVDpp, user and item factors are randomly initialized according to a normal distribution
+--> can be tuned using the init_mean and init_std_dev parameters
+"""
+
+
 class MatrixFactorizationAlgo:
 
