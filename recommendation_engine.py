a8bc3a6aecb8c07cb2a07c2d16abaa8b28479f0f
hanglinzhou <hanglin.zhou@richmond.edu>
2020-04-30 15:50:34 -0700

    added MF algo (untuned) and recommendation outline

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -1,10 +1,12 @@
 from data_processor import DataProcessor
 from surprise import NormalPredictor
-
+from matrix_factorization_algo import MatrixFactorizationAlgo
+from Evaluator import Evaluator
 #load dataset
 data_processor = DataProcessor()
 data_processor.SetDataPath('./ml-latest-small/')
 ratings = DataProcessor.LoadRating(data_processor)
+#construct evaluator
 
 #call MFalgo generate algo
 #call KNNalgo generate algo
@@ -12,8 +14,8 @@ ratings = DataProcessor.LoadRating(data_processor)
 #use random as our basline here
 Random = NormalPredictor()
 # evaluator.AddAlgorithm(Random, "Random")
-
-
+mf_algo = MatrixFactorizationAlgo()
+mf_algo_dict = mf_algo.generate_algorithms(ratings)
+for key in mf_algo_dict:
+    evaluator.AddAlgorithm(mf_algo_dict[key], key)
 # evaluate
-
-#
